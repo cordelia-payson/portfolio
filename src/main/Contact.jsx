8c54fdf873f3c8612aed971aeac77e7e35c4c28a
Cordelia Payson <cordelia.payson@gmail.com>
2023-02-01 13:58:01 -0500

    added contact buttons with links

--- src/main/Contact.jsx
+++ src/main/Contact.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 function Contact() {
   return (
-    <div>
+    <div className="flex flex-col">
       <h1 className="text-5xl font-bold m-5">Contact</h1>
+      <div className="flex flex-row">
+        <a href="mailto:cordelia.payson@gmail.com" target="_blank" rel="noreferrer noopener">
+          <button type="button" className="btn btn-primary w-fit m-3 normal-case">cordelia.payson@gmail.com</button>
+        </a>
+        <a href="https://www.linkedin.com/in/cordelia-payson/" target="_blank" rel="noreferrer noopener">
+          <button type="button" className="btn btn-primary w-fit m-3 normal-case">LinkedIn</button>
+        </a>
+        <a href="https://github.com/cordelia-payson" target="_blank" rel="noreferrer noopener">
+          <button type="button" className="btn btn-primary w-fit m-3 normal-case">GitHub</button>
+        </a>
+      </div>
+
     </div>
   );
 }
