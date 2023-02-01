d67fb006971251392a37de65e84519f544d1af18
Cordelia Payson <cordelia.payson@gmail.com>
2023-02-01 12:37:48 -0500

    added project examples

--- src/main/Projects.jsx
+++ src/main/Projects.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import SingleProject from '../project/SingleProject.jsx';
 import vh from '../project/vh.png';
+import thecampsite from '../project/thecampsite.png';
 
 function Projects() {
   return (
-    <div className="display-flex flex-col">
-      <h1 className="text-5xl font-bold">Projects</h1>
-      <SingleProject name="Visualizing History" description="description" link="https://github.com/cordelia-payson/visualizing-history" photo={vh} />
+    <div className="flex flex-col">
+      <h1 className="text-5xl font-bold m-5">Projects</h1>
+      <div className="flex flex-row m-0">
+        <SingleProject name="Visualizing History" description="Visualizing History was created as a result of my strong desire for a reliabe source for images of historical items without any visual clutter. This site is for readers, writers, and anyone with a love of history. Images are pulled from musuem collections for results that are more tailored than a Google search. the" link="https://github.com/cordelia-payson/visualizing-history" photo={vh} />
+        <SingleProject name="TheCampSite" description="TheCampSite allows users to review campsites and find campsites based on their location. Users can log in with their Google account and save campsites they are interested. I worked with five other teammates to bring this project to life. I was the UI owner." link="https://github.com/BO-BiscayBay/TheCampSite" photo={thecampsite} />
+      </div>
+
     </div>
   );
 }
--- src/project/SingleProject.jsx
+++ src/project/SingleProject.jsx
@@ -4,14 +4,14 @@ import GithubIcon from './GithubIcon.jsx';
 function SingleProject(props) {
   const { name, link, description, photo } = props;
   return (
-    <div className="card lg:card-side bg-base-100 shadow-xl m-10">
-      <figure className="w-3/5"><img src={photo} alt="Album" /></figure>
-      <div className="card-body">
+    <div className="card w-96 bg-base-100 shadow-xl m-10">
+      <figure className=""><img src={photo} alt="Album" /></figure>
+      <div className="card-body ">
         <h2 className="card-title">{name}</h2>
-        <p>{description}</p>
+        <p className="">{description}</p>
         <div className="card-actions justify-end">
           <a href={link} alt="github link" target="_blank" rel="noreferrer noopener">
-            <GithubIcon />
+            <GithubIcon className="transform transition duration-500 hover:scale-125 hover:shadow-2xl" />
           </a>
 
         </div>
