898c102f9d297c7fa5d876833896f3a7bd38e149
Cordelia Payson <cordelia.payson@gmail.com>
2023-02-01 12:57:49 -0500

    starting contact section

--- src/main/Contact.jsx
+++ src/main/Contact.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-function Contact() {}
+function Contact() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold m-5">Contact</h1>
+    </div>
+  );
+}
 
 export default Contact;
