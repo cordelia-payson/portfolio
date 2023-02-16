ff5ddf165e1d3cd0b1692696ae7a5c6a193953ed
Cordelia Payson <cordelia.payson@gmail.com>
2023-02-16 15:59:53 -0500

    removed react-pdf because it was not workign well

--- src/navbar/Resume.jsx
+++ src/navbar/Resume.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
 import ResumePic from '../images/resume.jpg';
-import ResumePdf from '../../public/resume.pdf';
 
 function Resume() {
   return (
@@ -14,11 +12,7 @@ function Resume() {
       <div className="modal">
         <div className="modal-box relative">
           <label htmlFor="my-modal-5" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-          {/* <img src={ResumePic} alt="resume" className="" /> */}
-          {/* <Document filed={ResumePdf}>
-            <Page pageNumber={1} />
-          </Document> */}
-          <iframe src={ResumePdf} />
+          <img src={ResumePic} alt="resume" className="" />
           <a href="resume.pdf" download="CordeliaPaysonResume.pdf">
             <button className="btn" type="button">Download</button>
           </a>
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,34 @@
 const path = require('path');
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         loader: 'babel-loader',
-        options: { presets: ['@babel/env'] },
       },
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
+        test: /\.(gif|png|jpe?g|svg)$/,
         type: 'asset/resource',
         generator: {
           filename: 'images/[name][ext]',
         },
       },
-      {
-        test: /\.(pdf)$/,
-        type: 'asset/source',
-        use: 'file-loader?name=[path][name].[ext]',
-      },
     ],
   },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
-  },
+  // resolve: { extensions: ['*', '.js', '.jsx'] },
+  devtool: 'eval-cheap-module-source-map',
   devServer: {
     static: {
       directory: path.join(__dirname, 'public'),
@@ -43,14 +36,4 @@ module.exports = {
     compress: true,
     port: 3000,
   },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.join(path.dirname(require.resolve('pdfjs-dist/package.json')), 'cmaps'),
-          to: 'cmaps/',
-        },
-      ],
-    }),
-  ],
 };
