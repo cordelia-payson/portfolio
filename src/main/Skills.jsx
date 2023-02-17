e4d7844909a721b2f06d195d525fa1a5ee9c5c6a
Cordelia Payson <cordelia.payson@gmail.com>
2023-02-17 11:03:30 -0500

    added skills section

---
+++ src/main/Skills.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Skills() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <h1 className="text-5xl font-bold">Skills</h1>
+        <div className="flex w-full">
+          <div>Javascript</div>
+          <div>React</div>
+          <div>CSS</div>
+          <div>HTML</div>
+          <div className="divider divider-horizontal" />
+          <div>Written Communication</div>
+          <div>Agile Methodologies</div>
+          <div>Research</div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
