bd570204fe946d5e67572a4b2c49d14a70ebe091
Cordelia Payson <cordelia.payson@gmail.com>
2023-02-23 12:00:25 -0500

    resume is now larger and responsive to screen size

--- src/navbar/Resume.jsx
+++ src/navbar/Resume.jsx
@@ -11,12 +11,12 @@ function Resume() {
       {/* Put this part before </body> tag */}
       <input type="checkbox" id="my-modal-5" className="modal-toggle" />
       <div className="modal">
-        <div className="modal-box relative bg-white">
+        <div className="modal-box relative bg-white max-w-4xl">
           <a href="resume.pdf" download="CordeliaPaysonResume.pdf">
             <button className="btn btn-sm absolute left-2 top-2 bg-primary text-primary-content border-none" type="button">Download</button>
           </a>
           <label htmlFor="my-modal-5" className="btn btn-sm btn-circle absolute right-2 top-2 bg-primary text-primary-content border-none">✕</label>
-          <img src={ResumePic} alt="resume" className="" />
+          <img src={ResumePic} alt="resume" className="max-w-full" />
         </div>
       </div>
     </div>
