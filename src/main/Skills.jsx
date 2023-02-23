04b1e095977169d62d8be8f609715a333040b96a
Cordelia Payson <cordelia.payson@gmail.com>
2023-02-23 10:44:29 -0500

    updated skills layout

--- src/main/Skills.jsx
+++ src/main/Skills.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import SkillsCard from './SkillsCard.jsx';
 
 function Skills() {
-  const techSkills = ['Javascript', 'React', 'CSS', 'HTML', 'SQL/noSQL', 'Redux'];
+  const frontSkills = ['Javascript', 'React', 'CSS', 'HTML', 'Redux', 'Figma'];
+  const backSkills = ['MongoDB', 'PostgreSQL', 'Express', 'APIs'];
   const softSkills = ['Agile Methodology', 'Problem Solving', 'Written and Oral Communication', 'Research'];
 
   return (
     <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row">
+      <div>
         <div>
-          <h1 className="text-5xl font-bold">Tech Stack</h1>
+          <h1 className="text-5xl font-bold text-center m-4">Skills</h1>
+          <div className="flex flex-col lg:flex-row">
+            <SkillsCard name="Frontend" skills={frontSkills} />
+            <SkillsCard name="Backend" skills={backSkills} />
+            <SkillsCard name="Soft Skills" skills={softSkills} />
+          </div>
         </div>
       </div>
     </div>
--- src/main/SkillsCard.jsx
+++ src/main/SkillsCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 function SkillsCard(props) {
-  const { skill } = props;
+  const { name, skills } = props;
 
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-96 bg-secondary text-primary-content m-3 shadow-2xl">
       <div className="card-body">
-        <h2 className="card-title">{skill}</h2>
+        <h2 className="card-title text-xl">{name}</h2>
+        {skills.map((skill) => <p>> {skill}</p>)}
       </div>
     </div>
   );
