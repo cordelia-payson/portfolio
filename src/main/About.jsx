e7cf40c2456cd6ad3bf8328f1ce88b37e8b69001
Cordelia Payson <cordelia.payson@gmail.com>
2023-05-03 10:19:26 -0400

    updated About section

--- src/main/About.jsx
+++ src/main/About.jsx
@@ -9,8 +9,8 @@ function About() {
         <div>
           <h1 className="text-5xl font-bold">About Me</h1>
           <p className="py-6"> My name is Cordelia Payson and I am a fullstack software engineer. I am enthusiastic about problem solving, writing clean code, and learning new tech. </p>
-          <p className="py-6">Excited about learning new languages, traveling, cooking, and reading a good book</p>
-          <p className="py-6">Currently learning.... Typescript</p>
+          <p className="py-6">After graduating college with a BA in European Studies I spent several years working a variety of jobs, searching for the right career. I had considered software engineering previously, but it wasn't until the summer of 2022 that I felt ready to pursue it as a career. I finally decided to enroll in the Hack Reactor 12-week software engineering immersive and learned full-stack development through 1,000+ hours of coding. </p>
+          <p className="py-6">Interested in my background or want to chat about creating the perfect React component? Send me an email or connect on LinkedIn!</p>
         </div>
       </div>
     </div>
